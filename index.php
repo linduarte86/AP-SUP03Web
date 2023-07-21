37ccc5b707f975564e65c3d6c739dd4865faefbc
linduarte86 <linduarte86@gmail.com>
2023-07-21 01:35:32 -0300

    Update

--- index.php
+++ index.php
@@ -8,17 +8,44 @@
 </head>
 
 <body>
-    <h1>Enviando um E-mail</h1>
-    <?php
-    // The message
-    $message = "Line 1\r\nLine 2\r\nLine 3";
-
-    // In case any of our lines are larger than 70 characters, we should use wordwrap()
-    $message = wordwrap($message, 70, "\r\n");
-
-    // Send
     
-    mail('linduarte86@hotmail.com', 'My Subject', $message);
+    <?php
+    require_once('src/PHPMailer.php');
+    require_once('src/SMTP.php');
+    require_once('src/Exception.php');
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    $email = new PHPMailer(true);
+
+    try {
+        $email->SMTPDebug = SMTP::DEBUG_SERVER;
+        $email->isSMTP();
+        $email->Host = 'smtp-mail.outlook.com';
+        $email->SMTPAuth = true;
+        $email->Username = 'linduarte86@hotmail.com';
+        $email->Password = 'ljc778963ljc';
+        $email->Port = 587;
+
+        $email->setFrom('linduarte86@hotmail.com');
+        $email->addAddress('linduarte86@gmail.com');
+
+        $email->isHTML(true);
+        $email->Subject = 'Teste de envio de email';
+        $email->Body = '<h2>O e-mail foi enviado com sucesso!</h2>';
+        $email->AltBody = 'O e-mail foi enviado com sucesso!';
+
+        if ($email->send()) {
+            echo 'E-mail enviado com sucesso!';
+        } else {
+            echo 'E-mail não enviado!';
+        }
+
+    } catch (Exception $e) {
+        echo "Erro ao enviar e-mail: {$email->ErrorInfo}";
+    }
     ?>
 </body>
 
